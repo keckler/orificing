e695d90524b463d20ccbe0fe872b11bddd3d0010
Chris Keckler <keckler@berkeley.edu>
2017-07-19 14:15:26 -0500

    updated constraint evaluations to loop over all power distributions

--- orificing.m
+++ orificing.m
@@ -195,46 +195,51 @@ while j < length(nbins)+1
     
     %check if bounds are tight
     tol = 0.001; %tolerance for checking
-    
-    display('------------------')
-    
-    if isnan(T_out(1)) == 1
-        display('problem infeasible')
-    else
-        if sum(T_out >= (T_in+dT_max)-tol*(T_in+dT_max))
-            display('max temp increase constraint is tight!')
+    i = 1;
+    while i < length(powerDetectorFiles) + 1 %cycle through all power profiles
+        display('------------------')
+        display('step')
+        disp(i)
+
+        if isnan(T_out(1,1)) == 1
+            display('problem infeasible')
         else
-            display('max temp increase constraint is loose')
+            if sum(T_out(:,i) >= (T_in+dT_max)-tol*(T_in+dT_max))
+                display('max temp increase constraint is tight!')
+            else
+                display('max temp increase constraint is loose')
+            end
+
+            if T_out_mixed(i) < T_out_bar(1)-T_out_bar_tol+0.1
+                display('mixed outlet temp constraint is lower tight!')
+            elseif T_out_mixed > T_out_bar(1)+T_out_bar_tol-0.1
+                display('mixed outlet temp constraint is upper tight!')
+            else
+                display('mixed outlet temp constraint is loose')
+            end
+
+            if sum(dP(:,i) >= dP_max-tol*dP_max)
+                display('max pressure drop constraint is tight!')
+            else
+                display('max pressure drop constraint is loose')
+            end
+
+            if sum(v(:,i) >= v_max-tol*v_max)
+                display('max flow velocity constraint is tight!')
+            else
+                display('max flow velocity constraint is loose')
+            end
+
+            if sum(m(:,i) <= 0.01)
+                display('min flowrate constraint is tight!')
+            else
+                display('min flowrate constraint is loose')
+            end
         end
-        
-        if T_out_mixed < T_out_bar(1)-T_out_bar_tol+0.1
-            display('mixed outlet temp constraint is lower tight!')
-        elseif T_out_mixed > T_out_bar(1)+T_out_bar_tol-0.1
-            display('mixed outlet temp constraint is upper tight!')
-        else
-            display('mixed outlet temp constraint is loose')
-        end
-    
-        if sum(dP >= dP_max-tol*dP_max)
-            display('max pressure drop constraint is tight!')
-        else
-            display('max pressure drop constraint is loose')
-        end
-    
-        if sum(v >= v_max-tol*v_max)
-            display('max flow velocity constraint is tight!')
-        else
-            display('max flow velocity constraint is loose')
-        end
-    
-        if sum(m <= 0.01)
-            display('min flowrate constraint is tight!')
-        else
-            display('min flowrate constraint is loose')
-        end
+
+        display('------------------')
+        i = i + 1;
     end
-    
-    display('------------------')
     
     %plot T_out, power, flowrate
     figure;
