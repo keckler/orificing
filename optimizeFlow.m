3e7dac0cb0aab7616f8ae41d67aa3c2f0388c062
Chris Keckler <keckler@berkeley.edu>
2017-12-05 10:03:29 -0800

    altered the cvx optimization files to print out the ampl model as it sets up the cvx problem

--- optimizeFlow.m
+++ optimizeFlow.m
@@ -1,9 +1,23 @@
-function [m, cvx_cputime, cvx_optbnd, cvx_optval, cvx_slvitr, cvx_slvtol, cvx_status] = optimizeFlow(alpha, j, T_out_bar, T_out_bar_tol, T_in, dT_max, rho, A_flow, P_w, v_max, f_novendstern, H, dP_max, nbins, zones, map, ncols, adjacent_max_diff)
+function [m, cvx_cputime, cvx_optbnd, cvx_optval, cvx_slvitr, cvx_slvtol, cvx_status] = optimizeFlow(alpha, j, T_out_bar, T_out_bar_tol, T_in, dT_max, rho, A_flow, P_w, v_max, f_novendstern, H, dP_max, nbins, zones, map, ncols, adjacent_max_diff, fm)
 
 cvx_precision best
 cvx_begin
     variables m(size(alpha)) x(nbins(j)) %inverse flowrate, dummies
 
+    fprintf(fm, 'var m{i in 1..%i, j in 1..%i};\n', length(alpha(:,1)), length(alpha(1,:)));
+    fprintf(fm, 'var x{i in 1..%i} >= 0;\n', nbins(j));
+    fprintf(fm, 'var step1ave;\n');
+    fprintf(fm, 'var step2ave;\n');
+    fprintf(fm, 'var step3ave;\n');
+    fprintf(fm, 'var step4ave;\n');
+    
+    fprintf(fm, 'minimize variance: (sum{i in 1..%i} ( (m[i,1]/alpha[i,1]) - step1ave)^2) + (sum{i in 1..%i} ( (m[i,2]/alpha[i,2]) - step2ave)^2) + (sum{i in 1..%i} ( (m[i,3]/alpha[i,3]) - step3ave)^2) + (sum{i in 1..%i} ( (m[i,4]/alpha[i,4]) - step4ave)^2);\n', length(m(:,1)), length(m(:,1)), length(m(:,1)), length(m(:,1)));
+    
+    fprintf(fm, 'subject to s1ave: step1ave = (sum{i in 1..%i} m[i,1]/alpha[i,1])/%i;\n', length(m(:,1)), length(m(:,1)));
+    fprintf(fm, 'subject to s2ave: step2ave = (sum{i in 1..%i} m[i,2]/alpha[i,2])/%i;\n', length(m(:,2)), length(m(:,2)));
+    fprintf(fm, 'subject to s3ave: step3ave = (sum{i in 1..%i} m[i,3]/alpha[i,3])/%i;\n', length(m(:,3)), length(m(:,3)));
+    fprintf(fm, 'subject to s4ave: step4ave = (sum{i in 1..%i} m[i,4]/alpha[i,4])/%i;\n', length(m(:,4)), length(m(:,4)));
+    
     i = 1;
     obj = 0;
     while i < length(alpha(1,:))+1
@@ -21,13 +35,37 @@ cvx_begin
     m/rho/A_flow <= v_max %maximum flow velocity in assembly
     (1.29+2.01+0.363+0.41+0.098+0.79+1.32)*m.^2/2/rho/A_flow^2 + (1.0+1.0+0.022+0.0024+0.000082+0.00035+f_novendstern*H/(4*A_flow/P_w))*m.^2/2/rho/A_flow^2 + rho*9.81*H <= dP_max %maximum pressure drop in assembly, form+friction+gravity, using generic values for form and friction losses from p281 of Waltar
 
+    fprintf(fm, 'subject to mixedUpper1: (sum{i in 1..%i} m[i,1]) >= (sum{i in 1..%i} alpha[i,1])/(T_out_bar + T_out_bar_tol - T_in);\n', length(m(:,1)), length(m(:,1)));
+    fprintf(fm, 'subject to mixedUpper2: (sum{i in 1..%i} m[i,2]) >= (sum{i in 1..%i} alpha[i,2])/(T_out_bar + T_out_bar_tol - T_in);\n', length(m(:,2)), length(m(:,2)));
+    fprintf(fm, 'subject to mixedUpper3: (sum{i in 1..%i} m[i,3]) >= (sum{i in 1..%i} alpha[i,3])/(T_out_bar + T_out_bar_tol - T_in);\n', length(m(:,3)), length(m(:,3)));
+    fprintf(fm, 'subject to mixedUpper4: (sum{i in 1..%i} m[i,4]) >= (sum{i in 1..%i} alpha[i,4])/(T_out_bar + T_out_bar_tol - T_in);\n', length(m(:,4)), length(m(:,4)));
+    fprintf(fm, 'subject to mixedLower1: (sum{i in 1..%i} m[i,1]) <= (sum{i in 1..%i} alpha[i,1])/(T_out_bar - T_out_bar_tol - T_in);\n', length(m(:,1)), length(m(:,1)));
+    fprintf(fm, 'subject to mixedLower2: (sum{i in 1..%i} m[i,2]) <= (sum{i in 1..%i} alpha[i,2])/(T_out_bar - T_out_bar_tol - T_in);\n', length(m(:,2)), length(m(:,2)));
+    fprintf(fm, 'subject to mixedLower3: (sum{i in 1..%i} m[i,3]) <= (sum{i in 1..%i} alpha[i,3])/(T_out_bar - T_out_bar_tol - T_in);\n', length(m(:,3)), length(m(:,3)));
+    fprintf(fm, 'subject to mixedLower4: (sum{i in 1..%i} m[i,4]) <= (sum{i in 1..%i} alpha[i,4])/(T_out_bar - T_out_bar_tol - T_in);\n', length(m(:,4)), length(m(:,4)));
+    fprintf(fm, 'subject to maxincrease1 {i in 1..%i}: m[i,1]/alpha[i,1] >= 1/dT_max;\n', length(m(:,1)));
+    fprintf(fm, 'subject to maxincrease2 {i in 1..%i}: m[i,2]/alpha[i,2] >= 1/dT_max;\n', length(m(:,2)));
+    fprintf(fm, 'subject to maxincrease3 {i in 1..%i}: m[i,3]/alpha[i,3] >= 1/dT_max;\n', length(m(:,3)));
+    fprintf(fm, 'subject to maxincrease4 {i in 1..%i}: m[i,4]/alpha[i,4] >= 1/dT_max;\n', length(m(:,4)));
+    fprintf(fm, 'subject to maxflow1 {i in 1..%i}: m[i,1]/(rho*A_flow) <= v_max;\n', length(m(:,1)));
+    fprintf(fm, 'subject to maxflow2 {i in 1..%i}: m[i,2]/(rho*A_flow) <= v_max;\n', length(m(:,2)));
+    fprintf(fm, 'subject to maxflow3 {i in 1..%i}: m[i,3]/(rho*A_flow) <= v_max;\n', length(m(:,3)));
+    fprintf(fm, 'subject to maxflow4 {i in 1..%i}: m[i,4]/(rho*A_flow) <= v_max;\n', length(m(:,4)));
+    %fprintf(fm, 'subject to pdrop1 {i in 1..%i}: (1.29+2.01+0.363+0.41+0.098+0.79+1.32)*(m[i,1]^2)/(2*rho*A_flow^2) + (1.0+1.0+0.022+0.0024+0.000082+0.00035+f_novendstern*H/(4*A_flow/P_w))*(m[i,1]^2)/(2*rho*A_flow^2) + rho*9.81*H <= dP_max;\n', length(m(:,1)));
+    %fprintf(fm, 'subject to pdrop2 {i in 1..%i}: (1.29+2.01+0.363+0.41+0.098+0.79+1.32)*(m[i,2]^2)/(2*rho*A_flow^2) + (1.0+1.0+0.022+0.0024+0.000082+0.00035+f_novendstern*H/(4*A_flow/P_w))*(m[i,2]^2)/(2*rho*A_flow^2) + rho*9.81*H <= dP_max;\n', length(m(:,2)));
+    %fprintf(fm, 'subject to pdrop3 {i in 1..%i}: (1.29+2.01+0.363+0.41+0.098+0.79+1.32)*(m[i,3]^2)/(2*rho*A_flow^2) + (1.0+1.0+0.022+0.0024+0.000082+0.00035+f_novendstern*H/(4*A_flow/P_w))*(m[i,3]^2)/(2*rho*A_flow^2) + rho*9.81*H <= dP_max;\n', length(m(:,3)));
+    %fprintf(fm, 'subject to pdrop4 {i in 1..%i}: (1.29+2.01+0.363+0.41+0.098+0.79+1.32)*(m[i,4]^2)/(2*rho*A_flow^2) + (1.0+1.0+0.022+0.0024+0.000082+0.00035+f_novendstern*H/(4*A_flow/P_w))*(m[i,4]^2)/(2*rho*A_flow^2) + rho*9.81*H <= dP_max;\n', length(m(:,4)));
+    
     %constraints so flow in each assembly remains constant over all steps
     i = 2;
     while i < length(alpha(1,:))+1
         m(:,1) == m(:,i)
+        
+        fprintf(fm, 'subject to flowConstantOverSteps%i {i in 1..%i}: m[i,1] = m[i,%i];\n', i, length(m(:,1)), i);
+        
         i = i + 1;
     end
-
+    
     %constraints to remain in bins
     k = 1;
     while k < nbins(j)+1
@@ -36,6 +74,8 @@ cvx_begin
         while l < length(zones)+1
             if zp(l) == 0
                 m(l,1) == x(k)
+                
+                fprintf(fm, 'subject to chan%iinGroup%i: m[%i,1] = x[%i];\n', l, k, l, k);
             end
             l = l + 1;
         end
@@ -64,36 +104,54 @@ cvx_begin
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(nw,:)./alpha(nw,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(nw,:)./alpha(nw,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, nw, length(m(1,:)), center, center, nw, nw);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, nw, length(m(1,:)), center, center, nw, nw);
             end
 
             if isnan(ne) %if northeast assembly is not in reduced matrix, don't do anything
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(ne,:)./alpha(ne,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(ne,:)./alpha(ne,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, ne, length(m(1,:)), center, center, ne, ne);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, ne, length(m(1,:)), center, center, ne, ne);
             end
 
             if isnan(w) %if west assembly is not in reduced matrix, don't do anything
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(w,:)./alpha(w,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(w,:)./alpha(w,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, w, length(m(1,:)), center, center, w, w);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, w, length(m(1,:)), center, center, w, w);
             end
 
             if isnan(e) %if east assembly is not in reduced matrix, don't do anything
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(e,:)./alpha(e,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(e,:)./alpha(e,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, e, length(m(1,:)), center, center, e, e);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, e, length(m(1,:)), center, center, e, e);
             end
 
             if isnan(sw) %if southwest assembly is not in reduced matrix, don't do anything
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(sw,:)./alpha(sw,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(sw,:)./alpha(sw,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, sw, length(m(1,:)), center, center, sw, sw);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, sw, length(m(1,:)), center, center, sw, sw);
             end
 
             if isnan(se) %if southeast assembly is not in reduced matrix, don't do anything
             else
                 m(center,:)./alpha(center,:) >= 1/(1+adjacent_max_diff)*m(se,:)./alpha(se,:)
                 m(center,:)./alpha(center,:) <= 1/(1-adjacent_max_diff)*m(se,:)./alpha(se,:)
+                
+                fprintf(fm, 'subject to channel%iAdjacent%iGreater {i in 1..%i}: m[%i,i]/alpha[%i,i] >= (1/(1+adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, se, length(m(1,:)), center, center, se, se);
+                fprintf(fm, 'subject to channel%iAdjacent%iLess {i in 1..%i}: m[%i,i]/alpha[%i,i] <= (1/(1-adjacent_max_diff))*(m[%i,i]/alpha[%i,i]);\n', center, se, length(m(1,:)), center, center, se, se);
             end
         end
 
--- orificing.m
+++ orificing.m
@@ -7,9 +7,9 @@ cp = 1272; %average heat capacity of coolant, J/kg/K
 dP_max = 1E6; %maximum allowable pressure drop over core, Pa, limit taken from Qvist et al
 dT_max = 200; %maximum temp increase allowable, C
 f_novendstern = 0.021132; %friction factor in the Novendstern friction loss model, see p282 of Waltar
-groupingMethod = 'log'; %choose from 'lin', 'log', 'kmeans', 'kmedoids'
+groupingMethod = 'kmedoids'; %choose from 'lin', 'log', 'kmeans', 'kmedoids'
 H = 3.18; %height of rods, m
-nbins = [28]; %vector of number of orifice zones
+nbins = [32]; %vector of number of orifice zones
 P_w = 7.2062; %wetted perimeter per assembly, m
 powerDetectorFiles = {'~/Documents/work/ARC/serpent/BnB/BnB_det0','~/Documents/work/ARC/serpent/BnB/BnB_det1','~/Documents/work/ARC/serpent/BnB/BnB_det2','~/Documents/work/ARC/serpent/BnB/BnB_det3'}; %paths of Serpent detector files with lattice power detectors
 rho = 850; %coolant density at lowest point, kg/m^3
@@ -17,6 +17,22 @@ T_in = 355; %coolant inlet temperature, C
 T_out_bar = 510; %perfectly mixed coolant outlet plenum temperature, C
 T_out_bar_tol = 5; %tolerance on perfectly mixed coolant outlet temperature, C
 v_max = 12.0; %maximum coolant velocity allowed in assembly, m/s, limit taken from Qvist et al
+
+fm = fopen('orificing.mod', 'w');
+fd = fopen('orificing.dat', 'w');
+
+fprintf(fm, 'param A_flow := %f; \n',A_flow);
+fprintf(fm, 'param adjacent_max_diff := %f; \n',adjacent_max_diff);
+fprintf(fm, 'param dP_max := %f; \n',dP_max);
+fprintf(fm, 'param dT_max := %f; \n',dT_max);
+fprintf(fm, 'param f_novendstern := %f; \n',f_novendstern);
+fprintf(fm, 'param H := %f; \n',H);
+fprintf(fm, 'param P_w := %f; \n',P_w);
+fprintf(fm, 'param rho := %f; \n',rho);
+fprintf(fm, 'param T_in := %f; \n',T_in);
+fprintf(fm, 'param T_out_bar := %f; \n',T_out_bar);
+fprintf(fm, 'param T_out_bar_tol := %f; \n',T_out_bar_tol);
+fprintf(fm, 'param v_max := %f; \n',v_max);
 
 Q = readQ(powerDetectorFiles);
 
@@ -28,13 +44,20 @@ Q_ave = sum(Q,2)/length(powerDetectorFiles); %divide by number of steps to get a
 
 alpha = Q/cp; %kg-K/s
 
+fprintf(fm, 'param alpha{i in 1..%i,j in 1..%i};\n', length(alpha(:,1)), length(alpha(1,:)));
+fprintf(fd, 'param alpha: 1 2 3 4 := ');
+for row = 1:length(alpha(:,1))
+    fprintf(fd, '\n    %i %f %f %f %f', row, alpha(row,1), alpha(row,2), alpha(row,3), alpha(row,4));
+end
+fprintf(fd, ';\n');
+
 %optimize for each number of orifice zones
 j = 1;
 while j < length(nbins)+1
     nb = nbins(j);
     zones = binning(Q_ave, Q, nb, groupingMethod);
     
-    [m, cvx_cputime, cvx_optbnd, cvx_optval, cvx_slvitr, cvx_slvtol, cvx_status] = optimizeFlow(alpha, j, T_out_bar, T_out_bar_tol, T_in, dT_max, rho, A_flow, P_w, v_max, f_novendstern, H, dP_max, nbins, zones, map, ncols, adjacent_max_diff);
+    [m, cvx_cputime, cvx_optbnd, cvx_optval, cvx_slvitr, cvx_slvtol, cvx_status] = optimizeFlow(alpha, j, T_out_bar, T_out_bar_tol, T_in, dT_max, rho, A_flow, P_w, v_max, f_novendstern, H, dP_max, nbins, zones, map, ncols, adjacent_max_diff, fm);
 
     [T_out, T_out_mixed, dP, v] = calculateFinalParameters(Q, m, cp, T_in, rho, A_flow, f_novendstern, H, P_w);
     
@@ -64,3 +87,6 @@ while j < length(nbins)+1
     
     j = j + 1;
 end
+
+fclose(fm);
+fclose(fd);
